f5a2fbed79ebb10bb16c87d3360a302741c93b58
Amita <amitavmenon2012@gmail.com>
2018-01-04 10:59:24 -0600

    First commit

---
+++ src/Computer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Amita V Menon
+ *
+ */
+import java.util.Random;
+ 
+public class Computer {
+    // This function helps the computer do the selection among ROCK,
+    // PAPER,SCISSORS
+    // we have used Random class from java.util.
+    public int getInput() {
+        Random random = new Random();
+        int input = random.nextInt(3) + 1;
+        return input;
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,56 @@
+
+/**
+ * @author Amita V Menon
+ *
+ */
+import java.util.Scanner;
+public class Player {
+	private String name;//Represents Player Name
+    Scanner sc=new Scanner(System.in);
+    
+    public String getName() {
+    	return name;
+    }
+    public void setName(String name) {
+    	this.name=name;
+    }
+ public void askName() {
+         System.out.println("Please enter your name");
+         name=sc.next();
+ }
+ 
+ 
+ public int getInput() {
+     // This function lets the user do the selection among
+     // ROCK PAPER SCISSORS
+     // and return what has been selected
+     System.out.println("Select  ROCK  PAPER SCISSOR");
+
+     String input = sc.next();
+     input = input.toUpperCase();
+     char c = input.charAt(0);
+     // checking the 1st character in the input
+     if (c == 'R')
+         return Driver.ROCK;
+     else if (c == 'P')
+         return Driver.PAPER;
+     else if (c == 'S')
+         return Driver.SCISSORS;
+     else {
+         getInput();
+         return 0;
+     }
+ }
+
+ public boolean playAgain() {
+     // This function will ask if the user wants to
+     // play again and return true or false accordingly.
+     sc = new Scanner(System.in);
+     System.out.print("Do you want to play again? ");
+     String userInput = sc.nextLine();
+     userInput = userInput.toUpperCase();
+     return userInput.charAt(0) == 'Y';
+ }
+ 
+ 
+}
